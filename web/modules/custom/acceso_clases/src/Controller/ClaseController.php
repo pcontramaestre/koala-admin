753b3571bff8fa1911798ddd87659449e78e0946
Pablo Contramaestre <pablo@icreativadigital.com>
2023-11-17 14:06:43 -0400

    Embed class

---
+++ web/modules/custom/acceso_clases/src/Controller/ClaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\acceso_clases\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+
+class ClaseController extends ControllerBase {
+  public function contenidoClase(Request $request) {
+    $url_param = $request->query->get('url');
+    
+    // Asegúrese de validar y desinfectar la URL aquí.
+    $url = filter_var($url_param, FILTER_SANITIZE_URL);
+
+    // Servir de manera predeterminada un mensaje o una URL por defecto si es necesario.
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+      $url = ''; // O establecer una URL por defecto o presentar un mensaje de error adecuado.
+    }
+
+    return [
+      '#theme' => 'clase-page',
+      '#url' => $url,
+      '#cache' => ['max-age' => 0], // Deshabilitar caching para este contenido dinámico.
+    ];
+  }
+}
