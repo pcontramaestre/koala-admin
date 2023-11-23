8c7858ef18e801e8f60d0a0bb6dc9ce83d5b0eb0
Pablo Contramaestre <pablo@icreativadigital.com>
2023-11-23 00:41:17 -0400

    Estrellas enviadas de profesores

--- web/modules/custom/clases_profesores/js/clases_profesores.js
+++ web/modules/custom/clases_profesores/js/clases_profesores.js
@@ -4,6 +4,23 @@
       $(context).find('.clases-del-profesor').once('clasesProfesoresBehavior').each(function () {
         // Este código se ejecutará una sola vez por cada elemento iframe.
         console.log('Se ha adjuntado el comportamiento a la clase del profesor.');
+        $('#activar-clase', context).click(function (e) {
+          e.preventDefault();
+          var idClase = $(this).data('id-clase');
+          var estado = $(this).data('estado');
+          if (estado == 'activar') {
+            $(this).data('estado', 'desactivar');
+            $(this).text('Finalizar clase');
+          }
+          if (estado == 'desactivar') {
+            $(this).data('estado', 'finalizada');
+            $(this).text('Clase finalizada');
+            // disabled = true
+            $(this).prop('disabled', true);
+            $('#asignar-estrella').prop('disabled', true);
+
+          }
+        });
       });
 
       $('#asignar-estrella', context).click(function (e) {
@@ -18,8 +35,8 @@
           },
           success: function (response) {
             // La lógica para actualizar la interfaz aquí
-            console.log(response);
-            console.log('Se ha asignado la estrella a la clase.');
+            // console.log(response);
+            // console.log('Se ha asignado la estrella a la clase.');
             /*
               Response:
               0: 
@@ -29,18 +46,18 @@
               selector: "#selector-para-mensajes"
               settings: null
             */
-            console.log(response[0].data);
+            // console.log(response[0].data);
             // Agregar mensaje de éxito en la interfaz, #selector-para-mensajes
             $('#selector-para-mensajes').html(response[0].data);
-
-            
-
           },
           error: function (jqXHR, textStatus, errorThrown) {
             console.log(errorThrown);
           }        
         });
       });
+
+      // Activar y desactivar clase #activar-clase
+      //console.log('Se ha adjuntado el comportamiento al botón de activar clase.');
     }
   };
 })(jQuery, Drupal);
