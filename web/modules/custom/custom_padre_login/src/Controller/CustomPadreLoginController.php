91f508662836d63a5f06bb1f74381983eb4dfd6c
Pablo Contramaestre <pcontramaestre@gmail.com>
2024-02-22 18:37:41 -0400

    Agregado módulo Custom Padre Login para permitir que el administrador inicie sesión como un padre seleccionado.

---
+++ web/modules/custom/custom_padre_login/src/Controller/CustomPadreLoginController.php
@@ -0,0 +1,40 @@
+<?php
+namespace Drupal\custom_padre_login\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+
+/**
+ * Controller para el inicio de sesión como padre.
+ */
+class CustomPadreLoginController extends ControllerBase {
+  /**
+   * Página de inicio de sesión como padre.
+   *
+   * @param int $uid
+   *   El ID del usuario padre seleccionado.
+   */
+  public function startSession($uid) {
+
+    //obtener usuario actual y detectar si tiene el rol de administrador
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $roles = $user->getRoles();
+    if (in_array('administrator', $roles)) {
+      $user = User::load($uid);
+      if ($user) {
+        user_login_finalize($user);
+        $this->messenger()->addMessage($this->t('Ahora estás logueado como el padre seleccionado.'));
+      }
+      return new RedirectResponse('/padres');
+    } else {
+      $this->messenger()->addMessage($this->t('No tienes permisos para realizar esta acción.'), MessengerInterface::TYPE_ERROR);
+      return new RedirectResponse('/');
+    }
+
+
+  }
+}
