8da63d9fab284bb9cde9c57bf3982a835aad57e6
Pablo Contramaestre <pcontramaestre@gmail.com>
2024-05-06 19:04:38 -0400

    Cambios en cancelar clases. Se agregar mensaje para que al cancelar la clase, se muestre un mensaje.

--- web/modules/custom/cancelar_clase/js/cancelar_clase.js
+++ web/modules/custom/cancelar_clase/js/cancelar_clase.js
@@ -1,24 +1,30 @@
 (function ($, Drupal) {
-  console.log('cancelar_clase.js');
-  $('#cancelarclase').click(function (e) {
-    e.preventDefault();
-    var href = $(this).attr('data-url');    
-    // Popup de confirmación.
-    if(confirm("¿Deseas cancelar esta clase? Si la eliminas, tendrás que volver a agendarla.")) {
-      // Si es confirmado, redirigir a la ruta.
-      window.location.href = href;
-    }
+  
+  jQuery(document).ready(function ($) {
+    console.log('cancelar_clase.js');
+    $('#cancelarclase').click(function (e) {
+      e.preventDefault();
+      var href = $(this).attr('data-url');    
+      // Popup de confirmación.
+      if(confirm("¿Deseas cancelar esta clase? Si la eliminas, tendrás que volver a agendarla.")) {
+        // Si es confirmado, redirigir a la ruta.
+        window.location.href = href;
+      }
+    });
+  
+    $('#cancelarclase').on('click', function (e) {
+      e.preventDefault();
+      var href = $(this).attr('data-url');    
+      // Popup de confirmación.
+      if(confirm("¿Deseas cancelar esta clase? Si la eliminas, tendrás que volver a agendarla.")) {
+        // Si es confirmado, redirigir a la ruta.
+        window.location.href = href;
+      }
+    });
   });
 
-  $('#cancelarclase').on('click', function (e) {
-    e.preventDefault();
-    var href = $(this).attr('data-url');    
-    // Popup de confirmación.
-    if(confirm("¿Deseas cancelar esta clase? Si la eliminas, tendrás que volver a agendarla.")) {
-      // Si es confirmado, redirigir a la ruta.
-      window.location.href = href;
-    }
-  });
+
+
 
 
   // $('a.cancelar-clase').on('click', function (e) {
@@ -33,21 +39,21 @@
   Drupal.behaviors.cancelarClaseModal = {
     attach: function (context, settings) {
 
-      console.log('cancelarClaseModal'  + context);
-      // Detecta el evento de click en el enlace con el id cancelarclase,
-      //y obtener el atributo data-url del enlace.
-      // despues confirmar e ir a la url.
+      // console.log('cancelarClaseModal'  + context);
+      // // Detecta el evento de click en el enlace con el id cancelarclase,
+      // //y obtener el atributo data-url del enlace.
+      // // despues confirmar e ir a la url.
 
-      $('#cancelarclase', context).once('cancelarClaseModal').on('click', function (e) {
-        console.log('cancelarClaseModal');
-        e.preventDefault();
-        var href = $(this).attr('data-url');
-        // Popup de confirmación.
-        if(confirm("¿Deseas cancelar esta clase? Si la eliminas, tendrás que volver a agendarla.")) {
-          // Si es confirmado, redirigir a la ruta.
-          window.location.href = href;
-        }
-      });
+      // $('#cancelarclase', context).once('cancelarClaseModal').on('click', function (e) {
+      //   console.log('cancelarClaseModal');
+      //   e.preventDefault();
+      //   var href = $(this).attr('data-url');
+      //   // Popup de confirmación.
+      //   if(confirm("¿Deseas cancelar esta clase? Si la eliminas, tendrás que volver a agendarla.")) {
+      //     // Si es confirmado, redirigir a la ruta.
+      //     window.location.href = href;
+      //   }
+      // });
       // $('a.cancelar-clase', context).once('cancelarClaseModal').on('click', function (e) {
       //   e.preventDefault();
       //   var href = $(this).attr('href');
--- web/modules/custom/cancelar_clase/src/Controller/CancelarClaseController.php
+++ web/modules/custom/cancelar_clase/src/Controller/CancelarClaseController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\Core\Entity\Query\QueryFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Define el controlador para el módulo cancelar_clase.
@@ -58,8 +59,58 @@ class CancelarClaseController extends ControllerBase {
           // Eliminar la clase.
           $node->delete();
           //return new JsonResponse(['status' => 'success', 'message' => 'Clase cancelada con éxito.']);
+          return new RedirectResponse('/en/padres');
+        } else {
+          $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+
+          $titulo = new TranslatableMarkup('Cancelar clase');
+          $mensaje = new TranslatableMarkup('¿Estás seguro de que quieres cancelar esta clase?');
+          $mensaje2 = new TranslatableMarkup('¿Estás seguro de que quieres cancelar esta clase?. Esta acción no se puede deshacer');
+          $texto_enlace_si = new TranslatableMarkup('Cancelar clase');
+          $texto_enlace_no = new TranslatableMarkup('Regresar');
+
+          $html = '<div class="cancelar-clase">';
+          $html .= '<h2>'.$titulo.'</h2>';
+          $html .= '<p class="text-center">'.$mensaje2.'</p>';
+          if ($language == 'es'){
+            $html .= '<div class="acciones d-flex"><a href="/cancelar-clase/' . $id . '/confirmar" class="btn-cancelar">'.$texto_enlace_si.'</a>';
+            $html .= '<a href="/padres/mi-calendario" class="btn-volver">'.$texto_enlace_no.'</a></div>';
+          } else {
+            $html .= '<div class="acciones d-flex"><a href="/cancelar-clase/' . $id . '/confirmar" class="btn-cancelar">'.$texto_enlace_si.'</a>';
+            $html .= '<a href="/en/padres/mi-calendario" class="btn-volver">'.$texto_enlace_no.'</a></div>';
+          }
+          $html .= '</div>';
+          // Mostrar html con el mensaje "¿Estás seguro de cancelar la clase gratis?"
+          return [
+            '#markup' => $html,
+          ];
+        }
+      }
+    }
+
+    return new JsonResponse(['status' => 'error', 'message' => 'Clase no encontrada.']);
+  }
+
+  public function cancelarConfirmar(Request $request) {  
+    $id = $request->get('id_clase');
+    if (!empty($id)) {
+      $node = Node::load($id);
+      if ($node instanceof Node && $node->bundle() == 'agendar_clase') {
+        // Verificar si el usuario tiene permiso para eliminar este nodo específico.
+        $access = $node->access('delete');
+        if (!$access) {
+          return new JsonResponse(['status' => 'error', 'message' => 'No tienes permisos para cancelar esta clase.']);
+        }
+        if ($node->get('field_tipo_clase')->value == 'Gratis') {          
+          // Eliminar la clase.
+          $node->delete();
+          //return new JsonResponse(['status' => 'success', 'message' => 'Clase cancelada con éxito.']);
           return new RedirectResponse('/padres');
         } else {
+
+          // mostrar un popup para confirmar si desea cancelar la clase.
+          
+
           $nids = $this->entityTypeManager->getListBuilder('node')->getStorage()->getQuery()
                 ->condition('type', 'asignar_clase_a_estudiante')
                 ->condition('field_seleccionar_clase_agendada', $id)
@@ -75,13 +126,10 @@ class CancelarClaseController extends ControllerBase {
           $node = Node::load($id);
           $node->delete();
 
-          return new RedirectResponse('/padres');
-          // Código adicional para clases que no son gratis.
-          //return new JsonResponse(['status' => 'error', 'message' => 'Las clases pagas no pueden ser canceladas directamente.']);
+          return new RedirectResponse('/en/padres');
         }
       }
     }
-
     return new JsonResponse(['status' => 'error', 'message' => 'Clase no encontrada.']);
   }
 
